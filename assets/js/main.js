f933dde4c43cf1dcfe3576d34bfdfb684acb4a1b
arjunrawal4 <arjunrawal4@gmail.com>
2017-03-27 22:00:50 -0500

    new theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,24 @@
 
 
 })(jQuery)
+
+$(document).ready(function(e) {
+    var width = $(document).width();
+
+    function goRight() {
+        $("#animate").animate({
+        left: width
+      }, 5000, function() {
+         setTimeout(goLeft, 50);
+      });
+    }
+    function goLeft() {
+        $("#animate").animate({
+        left: 0
+      }, 5000, function() {
+         setTimeout(goRight, 50);
+      });
+    }
+
+    setTimeout(goRight, 50);
+});
